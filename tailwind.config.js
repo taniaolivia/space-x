367656e0f9263c9b7e5cba958fa63bea3ee990ef
Tania Olivia <tania.olivia69@yahoo.com>
2023-01-17 23:45:42 +0100

    fix tailwind warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-   darkMode: false, // or 'media' or 'class'
+  content: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+   media: false,
    theme: {
      extend: {},
    },
