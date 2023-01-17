d28592a4ff6dc3421f48905c3c1a2e831d5287f4
Tania Olivia <tania.olivia69@yahoo.com>
2023-01-18 00:14:24 +0100

    fix footer position

--- src/components/FooterVue.vue
+++ src/components/FooterVue.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="bg-gray-600 w-screen py-2 text-white flex flex-row justify-center sticky bottom-0">
+  <div class="bg-gray-600 w-screen py-2 text-white flex flex-row justify-center fixed bottom-0">
     <span class="font-bold">@Space X - Tania OLIVIA</span>
   </div>
 </template>
