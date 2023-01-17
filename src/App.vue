a3a580d4cd04803b6a18c6fbeb84ff95bce5fc87
Tania Olivia <tania.olivia69@yahoo.com>
2023-01-17 01:43:21 +0100

    add footer and navbar to app file

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
-
+import NavbarVue from '@/components/NavbarVue.vue';
+import FooterVue from './components/FooterVue.vue';
 </script>
 
 <template>
   <div>
+    <navbar-vue></navbar-vue>
     <router-view></router-view>
+    <footer-vue></footer-vue>
   </div>
 </template>
